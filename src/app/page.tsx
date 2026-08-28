5e5f519da869da4007cd5372200dd5f0ddcc4bd9
Mateus Almeida <mateusalmeida0924@gmail.com>
2026-08-28 17:34:59 -0300

    feat: adicionar exportação dinâmica ao componente Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import { getColeta } from "@/lib/sheets";
 import { Dashboard } from "./_components/dashboard";
 
+
+export const dynamic = "force-dynamic";
+
+
 export default async function Home() {
   const itens = await getColeta();
 
